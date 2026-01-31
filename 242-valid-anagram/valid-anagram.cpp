649d08ab25263b6eb94329a0cadf1694736e352b
mayankdl03 <mayankdl2003@gmail.com>
2026-01-31 23:30:48 +0530

    Time: 6 ms (23.10%) | Memory: 9.6 MB (91.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        vector<int> alphabet(26);
-        for(int i=0;i<s.length();i++){
-            alphabet[s[i]-'a']++;
-        }
-        for(int i=0;i<t.length();i++){
-            alphabet[t[i]-'a']--;
-        }
-        for(auto it:alphabet){
-            if(it>0)return false;
-        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s!=t)return false;
         return true;
     }
 };
