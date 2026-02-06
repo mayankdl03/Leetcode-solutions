c2e710c73c355db92501db24d8329e9f70e486cc
mayankdl03 <mayankdl2003@gmail.com>
2026-02-06 15:35:12 +0530

    Time: 4 ms (39.35%) | Memory: 15.8 MB (25.15%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string ans="";
+        while(ss>>word){
+            reverse(word.begin(),word.end());
+            ans+=word+" ";
+        }
+        if(!ans.empty())ans.pop_back();
+        return ans;
+    }
+};
