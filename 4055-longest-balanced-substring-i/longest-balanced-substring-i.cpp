7ce47524edc21bee58018873a59cfd07140c4cc2
mayankdl03 <mayankdl2003@gmail.com>
2026-02-12 11:19:41 +0530

    Time: 47 ms (96.24%) | Memory: 25.3 MB (85.06%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.length();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            int maxFreq = 0;
+            int distinct = 0;
+
+            for(int j = i; j < n; j++) {
+                int idx = s[j] - 'a';
+                
+                if(freq[idx] == 0)
+                    distinct++;
+                
+                freq[idx]++;
+                maxFreq = max(maxFreq, freq[idx]);
+
+                int len = j - i + 1;
+
+                if(maxFreq * distinct == len)
+                    ans = max(ans, len);
+            }
+        }
+
+        return ans;
+    }
+};
